55f5a46189acf3ecf4271e090f6575879e730c55
Mukul <mukul.dharashivkar99@gmail.com>
2023-05-17 10:25:11 +0530

    first commit

---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div className="centered">
+    <p>Page not found!!!!</p>
+  </div>;
+};
+
+export default NotFound;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,71 @@
+import React, { Fragment, useEffect } from "react";
+import {
+  Switch,
+  Route,
+  useParams,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const DUMMY_QUOTES = [
+  { id: "p1", author: "Mukul", text: "Learning react is fun" },
+  { id: "p2", author: "Archana", text: "Learning react is great" },
+];
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No Quote Found</p>;
+  }
+
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
